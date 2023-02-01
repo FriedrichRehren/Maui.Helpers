5208e99a47c375847378ff3d3cafa5e9472ff966
Friedrich Rehren <development@friedrichrehren.de>
2023-02-01 02:06:13 +0100

    added DoubleToPercentConverter added TimeSpanToStringConverter

---
+++ src/Converters/DoubleToPercentConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace FriedrichRehren.Maui.Helpers.Converters;
+
+using System.Globalization;
+
+public class DoubleToPercentConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        System.Convert.ToInt32((double)value * 100);
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+         System.Convert.ToDouble(value) / 100;
+}
---
+++ src/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace FriedrichRehren.Maui.Helpers.Converters;
+
+using System.Globalization;
+
+public class TimeSpanToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        ((TimeSpan)value).ToString((string)parameter);
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        TimeSpan.ParseExact((string)value, (string)parameter, null);
+}
